8d0ff30f7a39a6a23fd499cbd7d42204b8dc5396
Luphia Chang <luphia@outlook.com>
2015-11-03 11:45:02 +0800

    Add guid and randomID function

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ dvalue.clone = function (obj) {
 	return rs;
 };
 
+dvalue.guid = function () {
+	var s4 = function() {
+		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+	};
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +s4() + '-' + s4() + s4() + s4();
+};
+
+dvalue.randomID = function (n) {
+	var ID = '';
+	var text = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	n = parseInt(n); 
+	if(!(n > 0)) { n = 8; }
+	while(ID.length < n) {
+		ID = ID.concat(text.charAt(parseInt(Math.random() * text.length)));
+	}
+	return ID;
+};
+
 dvalue.randomPick = function (arr, n) {
 	if(!Array.isArray(arr)) { return []; }
 	var tmp = this.clone(arr);
